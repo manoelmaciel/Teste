7bb2ad3dc62528ac5bdd552ca54ba79164671fed
Manoel Braz Maciel <manoelbraz.maciel@gmail.com>
2016-10-18 17:53:18 -0200

    menor lance Ok ... 2!

--- src/fontes_Versao1/Avaliador.java
+++ src/fontes_Versao1/Avaliador.java
@@ -15,17 +15,25 @@ package fontes_Versao1;
 public class Avaliador {
     
     private Double maiorDeTodos = Double.NEGATIVE_INFINITY;
+    private Double menorDeTodos = Double.POSITIVE_INFINITY;
 
     public void avalia(Leilao leilao) {
         for (Lance lance: leilao.getLances()) {
             if (lance.getValor() > maiorDeTodos) {
                     maiorDeTodos = lance.getValor();
-        }
+            } 
+            if (lance.getValor() < menorDeTodos) {
+            	menorDeTodos = lance.getValor();
+            }
         }
     }
 
     public Double getMaiorLance() {
         return maiorDeTodos;
+    }
+    
+    public Double getMenorLance() {
+    	return menorDeTodos;
     }
 
 }
